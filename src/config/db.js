0cfd240739dafbea9cda997ad9765d0e4168706c
Djessym BELGUERRI <djessym.belguerri@etu.udamail.fr>
2018-12-03 12:14:23 +0100

    ajout database

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: 'b62nvsssn',
+    username: 'uhb0za1wlzpzqsth',
+    password: 'wZqBr0cjKuelllqsEJG',
+    options: {
+        host: '	b62nvsssn-mysql.services.clever-cloud.com',
+        dialect: 'mysql'
+    }
+}
