6310aa6b6292b771b13c424e66f11afe19b5075f
Djessym BELGUERRI <djessym.belguerri@etu.udamail.fr>
2018-12-03 11:54:47 +0100

    rebuild

--- src/server.js
+++ src/server.js
@@ -1,15 +1,46 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express()
+//const models = require('./models/index');
 
-app.get('/',[example], function (req, res) {
-  res.send('Hello World!')
+// Decode json and x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Add a bit of logging
+app.use(morgan('short'))
+
+app.get('/', function(req, res) {
+  res.send('Hello world')
 })
 
-function example(req,res,next){
-	req.test= '15 DAYS'
-	next();
-}
-
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port 8080!')
+// Get all the users defined
+/*app.get('/', function (req, res) {
+  models.User.findAll()
+    .then((users) => {
+      res.json(users)
+    })
 })
+
+// Add a new user to the database
+app.post('/', function(req, res) {
+  models.User.create({
+    username: req.body.username
+  })
+    .then(() => {
+      res.send('User added !')
+    })
+})*/
+
+// Synchronize models
+//models.sequelize.sync().then(function() {
+  /**
+   * Listen on provided port, on all network interfaces.
+   * 
+   * Listen only when database connection is sucessfull
+   */
+  app.listen(process.env.PORT, function() {
+    console.log('Express server listening on port 3000');
+  });
+//});
